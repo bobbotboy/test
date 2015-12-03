c70b2bd22d964a3cf855550862f4e4fa58405cc4
Xyene <tbrindus@gmail.com>
2015-12-03 20:36:56 +0000

    This is my change
    Here I may include details about what I changed, why this change is significant and other blah blah blah

---
+++ NewFile.java
@@ -0,0 +1,11 @@
+public class NewFile
+{
+
+    public static void foo(int... a) {}
+
+    public static void main(String[] argv)
+    {
+        System.out.println("Hello, World!");
+        foo(5); foo(1, 2, 3, 4, 5);
+    }
+}
